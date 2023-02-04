fce7906b866f8ed75bd4b0e7e96e54419b7e71e4
Saba Muchauri <sabamuchauri@gmail.com>
2023-02-04 22:11:43 +0400

    finished home page and started private page

---
+++ assets/javascript/home.js
@@ -0,0 +1,4 @@
+document.querySelector('#redirect_button1')
+.addEventListener('click', () => {
+    window.location.href = 'piradi.html'
+});
---
+++ assets/javascript/piradi.js
@@ -0,0 +1,5 @@
+document.querySelector('#redirect_button2')
+.addEventListener('click', () => {
+    window.location.href = 'home.html'
+});
+console.log('awdnajhfashdb');
